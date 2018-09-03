d03a4d6d4d7ae06174f91bcea6a510760308140a
mathiasfranciaoktana <matiasf@oktana.io>
2018-09-03 17:01:45 -0300

    random wod functionality

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-﻿'use strict';
-var debug = require('debug');
+﻿var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,8 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -17,25 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
+// IMPORTANT: uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
 
 app.set('port', process.env.PORT || 3000);
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
-﻿'use strict';
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
+var db = require('../db/database.json');
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index.hbs', { title: 'Express' });
+});
+
+router.get('/wods', function (req, res) {
+    res.render('wods.hbs', { wods: encodeURIComponent(JSON.stringify(db)) });
+});
+
+router.get('/daily', function (req, res) {
+    res.render('daily.hbs', { title: 'daily' });
 });
 
 router.get('/error', function (req, res) {
