cd528acdbadf25504bc4a16bb35c4a0c80f3394e
mathiasfranciaoktana <matiasf@oktana.io>
2020-01-18 13:51:31 -0300

    commit from visual studio

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//made a new comment
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
